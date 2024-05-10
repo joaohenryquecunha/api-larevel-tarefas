fb096666f47b7e5d74908095bcf107b12251dd97
joao henryque <henryque_joao17@hotmail.com>
2024-05-09 23:53:51 -0300

    fix: ajuste

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -13,8 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
- 
+            App\Http\Middleware\CorsMiddleware::class;
     })
     ->withExceptions(function (Exceptions $exceptions) {
     
-    })->create();
+    })
+    ->create();
+
